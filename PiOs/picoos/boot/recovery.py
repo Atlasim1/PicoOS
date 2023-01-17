839873a91816b4ea141104c4d7bdfd1f53262ad3
Atlasim1 <70275601+Atlasim1@users.noreply.github.com>
2023-01-17 17:05:34 -0500

    Updated Recovery Program, Added More Features
    * Launch Config Editor
    * Launch Systoolrun
    * Launch Program (generic

--- PiOs/picoos/boot/recovery.py
+++ PiOs/picoos/boot/recovery.py
@@ -1,14 +1,14 @@
 import sys
 print("Welcome To Recovery Mode")
-print("Please Choose Action :\n 1 : Reboot\n 2 : Boot Main BIOS\n 3 : Exit To REPL")
+print("Please Choose Action :\n 1 : Reboot\n 2 : Boot Main BIOS\n 3 : Exit To REPL\n 4 : Launch Configuration Editor \n 5 : Launch SystemTools Runner\n 6 : Run Program")
 
 while True:
     SelectedAction = input("Recovery > ")
 
     if SelectedAction == "1":
-        import machine
+      #  import machine
         print("Rebooting ...")
-        machine.soft_reset()
+    #    machine.soft_reset()
         
     elif SelectedAction == "2":
         print ("Entering BIOS")
@@ -16,6 +16,15 @@ while True:
         
     elif SelectedAction == "3":
         sys.exit()
+        
+    elif SelectedAction == "4":
+        exec(open("/picoos/systools/confedit.py").read())
+        
+    elif SelectedAction == "5":
+        exec(open("/picoos/boot/SystoolRun.py").read())
+        
+    elif SelectedAction == "6":
+        exec(open(input("Run Program > ")).read())
         
     else :
         print("Invalid Choice")
