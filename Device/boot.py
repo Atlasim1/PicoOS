d1f6923099bf2f6359343a6750120ff46b77f635
Atlasim1 <70275601+Atlasim1@users.noreply.github.com>
2023-08-14 13:07:38 -0400

    Cleaned up service manager

--- Device/boot.py
+++ Device/boot.py
@@ -5,12 +5,13 @@ os.chdir("/")
 import cmd
 print("Loaded Modules")
 
+#Run Autochk if scheduled
 if open("/picoos/config/SYSTEM/boot/AUTOCHK_SCHD.cfg").read() == "True":
     print("Preparing Disk Repair")
     exec(open("/picoos/config/SYSTEM/boot/AUTOCHK_PATH.cfg").read())
 
 #Thread And Services Manager
-def servicemessage(message):
+def servicemessage(message): # service output function
     print("A Service on this device is reporting an error")
     servicemessage_wouldlike = input("Would you like to see this error?\n SERVICES (y / n) >>> ")
     if servicemessage_wouldlike == "y" or servicemessage_wouldlike == "yes":
@@ -18,23 +19,32 @@ def servicemessage(message):
         print("Message Receiving Done, You may now use your computer")
     else:
         print("Message Receiving Canceled, You may now use your computer")
-    
+        
+#get list of services needed to boot
 servicelistraw = open("/picoos/config/SYSTEM/services/BOOT_SERVICES.cfg","r").read()
 servicelist = []
 servicelist = servicelistraw.split("\n")
-runlistfile = "/picoos/service/runlist"
+
+runlistfilepath = "/picoos/service/runlist" #why is this here?
 
 import _thread
+try:
+    if open("/picoos/config/SYSTEM/services/NO_SERVICES_BOOT.cfg").read() == "False": 
+        def thread2():
+            for service in servicelist:
+                try:
+                    exec(open(f"/picoos/service/Services/{service}").read())
+                except Exception as Error:
+                    servicemessage(f"ERROR : {Error}")
+        _thread.start_new_thread(thread2, ())
+        print("Started Services")
+    elif open("/picoos/config/SYSTEM/boot/NO_SERVICES_BOOT.cfg").read() == "True":
+        print("Boot requested without services, Booting without services (System may be unstable")
+except OSError:
+    print("Missing File Error (Probably Missing Key?)")
+#---
 
-def thread2():
-    for service in servicelist:
-        try:
-            exec(open(f"/picoos/service/Services/{service}").read())
-        except Exception as Error:
-            servicemessage(f"ERROR : {Error}")
-_thread.start_new_thread(thread2, ())
-print("Started Services")
-
+#Selector/Bootmenu
 booted = True
 while booted == True:
     print("Please Choose Action : \n 1 : Boot Default OS\n 2 : Boot Custom OS\n 3 : List All Bootfiles\n 4 : More Options")
